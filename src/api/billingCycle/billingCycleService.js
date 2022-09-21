09af8b3d284deca226440d50517914371ed956ac
Guilherme <mttsguil@gmail.com>
2022-09-21 12:39:43 -0300

    add: att mongoose

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -22,4 +22,30 @@ billingCycle.route('get', (req, res, next) => {
 
 })
 
+billingCycle.route('count', (req, res, next)=> { 
+    billingCycle.count((error, value)=> { 
+        if(error) { 
+            res.status(500).json({errors: [error]})
+        } else { 
+            res.json({value})
+        }
+    })
+})
+
+billingCycle.route('summary', (req, res, next) => {
+    billingCycle.aggregate([{ 
+        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}} 
+    }, { 
+        $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
+    }, { 
+        $project: {_id: 0, credit: 1, debt: 1}
+    }], (error, result) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json(result[0] || {credit: 0, debt: 0})
+        }
+    })
+})
+
 module.exports = billingCycle
