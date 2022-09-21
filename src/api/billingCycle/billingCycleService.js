0766cd1f9cf937590776d6c591917e0c8def2994
Guilherme <mttsguil@gmail.com>
2022-09-21 11:55:51 -0300

    add: creat API REST

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -1,2 +1,25 @@
-const BillingCycle = require('./billingCycle')
+const billingCycle = require('./billingCycle')
 
+
+billingCycle.methods(['get', 'post', 'put', 'delete'])
+billingCycle.updateOptions({new: true, runValidators: true})
+
+billingCycle.route('get', (req, res, next) => {
+
+    billingCycle.find({}, (err, docs) => {
+
+        if(!err) {
+
+            res.json(docs)
+
+        } else {
+
+            res.status(500).json({errors: [error]})
+
+        }
+
+    })
+
+})
+
+module.exports = billingCycle
