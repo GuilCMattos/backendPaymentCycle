eeb030c8d4dad61c62f5e7a7581557beaff4150b
Guilherme <mttsguil@gmail.com>
2022-09-22 08:09:35 -0300

    add: handling error message

---
+++ src/api/common/errorHandler.js
@@ -0,0 +1,21 @@
+const _ = require('lodash')
+
+module.exports = (req, res, next) => { 
+    const bundle = res.locals.bundle 
+
+    if(bundle.errors) { 
+        const errors = parseErros(bundle.erros)
+        res.status(500).json({errors})
+    } else { 
+        next()
+    }
+}
+
+const parseErros = (nodeRestfulErrors) => { 
+    const errors = []
+
+    _.forIn(nodeRestfulErrors, error => errors.push(error.message))
+
+    return errors
+
+}
