a1d0c4adc64a5cbbc85dffccbe6910837b4c8093
Guilherme Mattos <91619478+GuilCMattos@users.noreply.github.com>
2022-09-26 09:54:31 -0300

    Authentication (#1)
    * add: user.js and authService.js
    
    * add: method login and validateToken
    
    * add: method login, singup and ValidateToken
    
    * add: auth.js
    
    * refec: routes.js to protected API

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,13 +1,27 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) { 
-    //Definir URL Base
 
-    const router = express.Router()
-    server.use('/api', router)
+    // Rotas protegidas por JWT
+   
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
 
-    // Rotas dos ciclos de Pagamentos
-    const billingCycle = require('../api/billingCycle/billingCycleService')
-    
-    billingCycle.register(router, '/billingCycles')
+    protectedApi.use(auth)
+
+    const BillingCycle = require('../api/billingCycle/billingCycleService')
+    BillingCycle.register(protectedApi, '/billingCycles')
+
+
+    // Rotas abertas
+
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.singup)
+    openApi.post('/validateToken', AuthService.validateToken)
+
 }
